bcb2c578190d9baa9c297252337268ea33343e69
Vitaliy Vostrikov <vitaliy@Vitaliys-iMac.local>
2013-11-27 18:08:26 +0400

    add selectors

--- app.js
+++ app.js
@@ -24,36 +24,42 @@ var cars = new CarsCollection();
 
 //Car view
 var CarView = Backbone.View.extend({
-    tagName: 'li',
-    template: _.template( $('#carsList').html() ),
-    initialize: function() {
-        this.render();
-    },
+    className: 'car-item',
+    template: _.template( $('#cars-List').html() ),
     render: function() {
         this.$el.html( this.template(this.model.toJSON()) );
         return this;
+    },
+    initialize: function() {
+        return this.render();
     }
 });
 
 //Cars view
 var CarsView = Backbone.View.extend({
-    tagName: 'ul',
+    className: 'cars-items',
+    length: function(){
+        return this.collection.models.length;
+    },
     render: function() {
         this.collection.each(function(oneCar) {
             var OneCarView = new CarView({model: oneCar});
             this.$el.append(OneCarView.render().el);
         }, this);
- 
         return this;
     },
     initialize: function() {
-        this.render();
+       this.render();
     }
 });
 var list = new CarsView({collection: cars});
 
-$(function() {
-    $('.js-carList').append(list.render().el);
+$('.js-show-list').one('click', function(){
+    list.render();
+    $('.js-tabs').css("visibility","visible");
+    $('.counter-all').append(list.length());
+    $('.js-car-List').append(list.el);
 });
+
 
 
